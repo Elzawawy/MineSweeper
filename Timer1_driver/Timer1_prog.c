6bd55a56d9abd51925816286544475357c1f02b4
Amr Elzawawy <amr22.elzawawy@ymail.com>
2018-11-27 13:41:13 +0200

    Added Naming Convention to Timer1 Prog.c file implementation.

--- Timer1_driver/Timer1_prog.c
+++ Timer1_driver/Timer1_prog.c
@@ -11,7 +11,7 @@
 #include "Timer1_config.h"
 #include "Timer1_private.h"
 
-u8 TIMER1_INITALIZE()
+void Timer1_voidInit()
 {
 	AssignBit(TCCR1A,WGM10,WGM10_VAL);
 	AssignBit(TCCR1A,WGM11,WGM11_VAL);
@@ -19,29 +19,25 @@ u8 TIMER1_INITALIZE()
 	AssignBit(TCCR1A,COM1A0,COM1A0_VAL);
 	AssignBit(TCCR1A,COM1B1,COM1B1_VAL);
 	AssignBit(TCCR1A,COM1B0,COM1B0_VAL);
-
-
 	AssignBit(TCCR1B,WGM12,WGM12_VAL);
 	AssignBit(TCCR1B,WGM13,WGM13_VAL);
-
 	AssignBit(TCCR1B,CS10,CS10_VAL);
 	AssignBit(TCCR1B,CS11,CS11_VAL);
 	AssignBit(TCCR1B,CS12,CS12_VAL);
-
 	AssignBit(TCCR1B,ICNC1,ICNC1_VAL);
 	AssignBit(TCCR1B,ICES1_VAL,ICES1_VAL);
 
-return NORMALSTATE;
+return Timer1_enuNormalState;
 }
 
-u8 TIMER1_SET_TCNT1(u16 Copy_u16TCNT1Val)
+u8 Timer1_u8setTCNT1(u16 Copy_u16TCNT1Val)
 {
 	TCNT1 = Copy_u16TCNT1Val;
-	return NORMALSTATE;
+	return Timer1_enuNormalState;
 }
-u8 TIMER1_GET_TCNT1(u16* u16_ptrtoVal)
+u8 Timer1_u8getTCNT1(u16* u16_ptrtoVal)
 {
 	*u16_ptrtoVal = TCNT1;
-	return NORMALSTATE;
+	return Timer1_enuNormalState;
 
 }
